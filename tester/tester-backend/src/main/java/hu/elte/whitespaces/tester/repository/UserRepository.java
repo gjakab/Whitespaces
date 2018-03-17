5e80686ba1744caf5ca60f4becf04f82d17cb270
kalmangergely <kgergely@inf.elte.hu>
2018-03-17 16:55:13 +0100

    Added UserRepository

---
+++ tester/tester-backend/src/main/java/hu/elte/whitespaces/tester/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package hu.elte.whitespaces.tester.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import hu.elte.whitespaces.tester.model.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    Optional<User> findById(Integer id);
+}
