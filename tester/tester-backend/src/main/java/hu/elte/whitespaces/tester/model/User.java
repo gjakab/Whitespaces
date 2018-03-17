b96ae1f1d703c4978156e5b2bf9c52598b72f864
kalmangergely <kgergely@inf.elte.hu>
2018-03-17 18:19:51 +0100

    I added getters and setters because they dont appear in the tests and the integration test didnt run

--- tester/tester-backend/src/main/java/hu/elte/whitespaces/tester/model/User.java
+++ tester/tester-backend/src/main/java/hu/elte/whitespaces/tester/model/User.java
@@ -54,7 +54,71 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private Role role;
 
-	public enum Role {
+	public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Test> getTests() {
+        return tests;
+    }
+
+    public void setTests(List<Test> tests) {
+        this.tests = tests;
+    }
+
+    public List<Test> getTestResults() {
+        return testResults;
+    }
+
+    public void setTestResults(List<Test> testResults) {
+        this.testResults = testResults;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public enum Role {
         STUDENT, TEACHER, ADMIN
     }
 }
