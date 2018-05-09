71a9f95f554c3e50dad6076e527b1026dc75bbbf
kalmangergely <kgergely@inf.elte.hu>
2018-05-09 21:40:17 +0200

    [Frontend-6] Added login, logout and auth logic

--- tester/tester-frontend/src/main/frontend/src/app/routing/app-routing.module.ts
+++ tester/tester-frontend/src/main/frontend/src/app/routing/app-routing.module.ts
@@ -4,13 +4,14 @@ import { SignUpComponent } from "../component/user/sign-up/sign-up.component";
 import { SignInComponent } from "../component/user/sign-in/sign-in.component";
 import { QuizListComponent } from "../component/user/teacher/quiz-list/quiz-list.component";
 import { NewQuizComponent } from "../component/user/teacher/new-quiz/new-quiz.component";
+import { AuthGuardTeacher } from "../service/guard/auth-guard-teacher.service";
 
 const appRoutes: Routes = [
-    { path: 'users/register', component: SignUpComponent},
+    { path: 'users/register', canActivate: [AuthGuardTeacher], component: SignUpComponent},
     { path: 'users/login', component: SignInComponent},
-    { path: 'users/quizlist', component: QuizListComponent},
-    { path: 'users/quizlist/:quizId', component: QuizListComponent},
-    { path: 'users/newquiz', component: NewQuizComponent},
+    { path: 'users/quizlist', canActivate: [AuthGuardTeacher], component: QuizListComponent},
+    { path: 'users/quizlist/:quizId', canActivate: [AuthGuardTeacher], component: QuizListComponent},
+    { path: 'users/newquiz', canActivate: [AuthGuardTeacher], component: NewQuizComponent},
 ];
 
 @NgModule({
